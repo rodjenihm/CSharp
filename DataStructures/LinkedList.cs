bd8ca224c51a51d545997e2226225144fbaa55e1
Данијел Митровић <rodjenihm@outlook.com>
2020-03-09 02:31:23 +0100

    Add LinkedList<T>. Implement Count, First and Last properties. Implement AddFirst, AddLast and Clear methods

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public partial class LinkedList<T>
+    {
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public LinkedListNode<T> First
+        {
+            get
+            {
+                return first;
+            }
+        }
+
+        public LinkedListNode<T> Last
+        {
+            get
+            {
+                return last;
+            }
+        }
+
+        public LinkedList()
+        {
+        }
+
+        public LinkedListNode<T> AddFirst(T value)
+        {
+            var newNode = new LinkedListNode<T>(value, previous: null, next: null);
+
+            if (first == null)
+            {
+                first = last = newNode;
+            }
+            else
+            {
+                newNode.next = first;
+                first.prev = newNode;
+                first = newNode;
+            }
+
+            count++;
+
+            return newNode;
+        }
+
+        public LinkedListNode<T> AddLast(T value)
+        {
+            var newNode = new LinkedListNode<T>(value, previous: null, next: null);
+
+            if (first == null)
+            {
+                first = last = newNode;
+            }
+            else
+            {
+                newNode.prev = last;
+                last.next = newNode;
+                last = newNode;
+            }
+
+            count++;
+
+            return newNode;
+        }
+
+        public void Clear()
+        {
+            var iter = first;
+
+            while (iter != null)
+            {
+                var temp = iter;
+                iter = iter.next;
+                temp.prev = null;
+                temp.next = null;
+            }
+
+            first = last = null;
+            count = 0;
+        }
+
+        private int count = 0;
+        private LinkedListNode<T> first = null;
+        private LinkedListNode<T> last = null;
+    }
+}
---
+++ DataStructures/LinkedListNode.cs
@@ -0,0 +1,56 @@
+﻿namespace DataStructures
+{
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode<T> Next
+        {
+            get
+            {
+                return next;
+            }
+        }
+
+        public LinkedListNode<T> Previous
+        {
+            get
+            {
+                return prev;
+            }
+        }
+
+        public T Value
+        {
+            get
+            {
+                return Value;
+            }
+
+            set
+            {
+                val = value;
+            }
+        }
+
+
+        public LinkedListNode()
+            : this(default, null, null)
+        {
+        }
+
+        public LinkedListNode(T value)
+            : this(value, null, null)
+        {
+        }
+
+        public LinkedListNode(T value, LinkedListNode<T> previous, LinkedListNode<T> next)
+        {
+            val = value;
+            prev = previous;
+            this.next = next;
+        }
+
+        internal LinkedListNode<T> next;
+        internal LinkedListNode<T> prev;
+        internal T val;
+    }
+}
