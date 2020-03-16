8ce1d940659017acdebdb6b3b5ef8574001c21a2
Данијел Митровић <rodjenihm@outlook.com>
2020-03-16 01:56:32 +0100

    Add MyLinq  static class: here I will implement my own versions of linq methods. Add List<T>

---
+++ DataStructures/Extensions/MyLing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Extensions
+{
+    public static class MyLing
+    {
+        public static int Count<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (source is ICollection<T> sourceAsCollection)
+            {
+                return sourceAsCollection.Count;
+            }
+
+            int counter = 0;
+
+            var e = source.GetEnumerator();
+
+            while (e.MoveNext())
+            {
+                counter++;
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ DataStructures/List.cs
@@ -0,0 +1,81 @@
+﻿using DataStructures.Extensions;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class List<T> : IList<T>, ICollection<T>, IEnumerable<T>
+    {
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count => count;
+
+        public int Capacity { get; set; } = 0;
+
+        public List()
+            : this(0)
+        {
+        }
+
+        public List(IEnumerable<T> collection)
+        {
+            Capacity = collection.Count();
+        }
+
+        public List(int capacity)
+        {
+            Capacity = capacity;
+            array = new T[capacity];
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int count = 0;
+        private int idx = -1;
+        private T[] array;
+    }
+}
