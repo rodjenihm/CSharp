3d48fc8a29ec1f74127a47551fb4a529cdd7e7bc
Данијел Митровић <rodjenihm@outlook.com>
2020-04-12 18:09:52 +0200

    Add Algorithms and Algorithms.Tests projects. Add StringOperation class. Implement FirstNonRepeatingCharacter method

---
+++ Algorithms.Tests/StringOperationsShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class StringOperationsShould
+    {
+        [Theory]
+        [InlineData("aaabcccdeeef", 'b')]
+        [InlineData("abcbad", 'c')]
+        [InlineData("abcabcabc", '_')]
+        public void ReturnFirstNonRepeatingCharacter(string inputString, char firstNonRepeatingCharacter)
+        {
+            var sut = new StringOperations();
+
+            Assert.Equal(sut.FirstNonRepeatingCharacter(inputString), firstNonRepeatingCharacter);
+        }
+    }
+}
---
+++ Algorithms/StringOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class StringOperations
+    {
+        /// <summary>
+        /// Given input string s this method returns first non-repeating character.
+        /// Returns '_' if there is no such character.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns>First non-repeating character in input string</returns>
+        public char FirstNonRepeatingCharacter(string s)
+        {
+            var dic = new Dictionary<char, bool>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dic.ContainsKey(s[i])) dic.Add(s[i], true);
+                else dic[s[i]] = false;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+                if (dic[s[i]] == true) return s[i];
+
+            return '_';
+        }
+    }
+}
